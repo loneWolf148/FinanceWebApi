ff717614bf20d834391a7a1151c0824117156023
deepanshishrimal <76088777+deepanshishrimal@users.noreply.github.com>
2021-01-07 22:42:12 +0530

    Add files via upload

---
+++ FinanceWebApi/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using FinanceWebApi.Model;
+
+namespace FinanceWebApi.Controllers
+{
+    [EnableCors(origins:"*",headers:"*",methods:"*")]
+    public class LoginController : ApiController
+    {
+        FinanceEntities db = new FinanceEntities();
+            
+        [HttpPost]
+        public HttpResponseMessage GetUser(Login log)
+        {
+            var display = db.Consumers.Where(m => m.UserName == log.UserName && m.Password == log.Password).FirstOrDefault();
+            if (display != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.Accepted, "Login Successfully");
+            }
+            
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid Username or Password");
+            }
+        }
+        [HttpPost]
+        public HttpResponseMessage GetAdmin(Login log)
+        {
+            var display = db.Admins.Where(m => m.AdminName == log.UserName && m.Password == log.Password).FirstOrDefault();
+            if (display != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.Accepted, "Admin Login Successfully");
+            }
+
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Invalid Name or Password");
+            }
+        }
+    }
+}
