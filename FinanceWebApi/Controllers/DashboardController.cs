65d170685e83e29d09067c45fa0232c2300ffd8d
yesmitha <59398991+yesmitha@users.noreply.github.com>
2021-01-08 10:20:30 +0530

    Update DashboardController.cs

--- FinanceWebApi/Controllers/DashboardController.cs
+++ FinanceWebApi/Controllers/DashboardController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -18,39 +18,63 @@ namespace FinanceWebApi.Controllers
         [HttpGet]
         public HttpResponseMessage GetProducts()
         {
-            
-                var productlist = from product in db.Products 
+            try
+            {
+                var productlist = from product in db.Products
                                   select new { product.ProductID, product.ProductName, product.ProductCost, product.ProductAvailability };
                 return Request.CreateResponse(HttpStatusCode.OK, productlist);
-            
+            }
+            catch
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Something Went Wrong");
+
+            }
+
         }
         [HttpGet]
-        public HttpResponseMessage GetCardDetails(string cardNo)
+        public HttpResponseMessage GetCardDetails(string id)
         {
-            var companyCard = from x in db.CompanyCards
-                              where x.CardNumber == cardNo
-                              join y in db.Cards on x.CardTypeNo equals y.CardTypeNo
-                              join z in db.Consumers on x.UserName equals z.UserName
-                              select new { x.CardNumber, z.Name, x.Validity, y.CardType };
-            return Request.CreateResponse(HttpStatusCode.OK, companyCard);
-            
+            try
+            {
+                var companyCard = from x in db.CompanyCards
+                                  where x.UserName == id
+                                  join y in db.Cards on x.CardTypeNo equals y.CardTypeNo
+                                  join z in db.Consumers on x.UserName equals z.UserName
+                                  select new { x.CardNumber, z.Name, x.Validity, y.CardType, Activated = DateTime.Compare(x.Validity, DateTime.Now) >= 0 };
+                return Request.CreateResponse(HttpStatusCode.OK, companyCard);
+            }
+            catch
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Something Went Wrong");
+
+            }
+
         }
+
         [HttpGet]
 
-        public HttpResponseMessage GetProductsPurchased(string cardNo)
+        public HttpResponseMessage GetProductsPurchased(string id)
         {
-            
-            var productspurchased = from a in db.Transactions
-                                    join b in db.CompanyCards on a.UserName equals b.UserName where b.CardNumber==cardNo
-                                    join c in db.Products on a.ProductID equals c.ProductID 
-                                    select new
-                                    {
-                                        c.ProductName,
-                                        c.ProductCost,
-                                        a.RemainingAmount
-                
-                                    };
-             return Request.CreateResponse(HttpStatusCode.OK, productspurchased);
+            try
+            {
+
+                var productspurchased = from a in db.Transactions
+                                        where a.UserName == id
+                                        join c in db.Products on a.ProductID equals c.ProductID
+                                        select new
+                                        {
+                                            c.ProductName,
+                                            a.PurchaseDate,
+                                            AmountPaid = c.ProductCost - a.RemainingAmount
+
+                                        };
+                return Request.CreateResponse(HttpStatusCode.OK, productspurchased);
+            }
+            catch
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Something Went Wrong");
+
+            }
 
 
         }
